6174862d5c094a60f2b1ffc1f5f1a847d385594e
Bryan de Bourbon <bryandebourbon@gmail.com>
2025-03-23 15:37:50 -0400

    Initial Commit

---
+++ UserDefaultsPro/UserDefaultsProApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsProApp.swift
+//  UserDefaultsPro
+//
+//  Created by Bryan de Bourbon on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserDefaultsProTests/UserDefaultsProTests.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultsProTests.swift
+//  UserDefaultsProTests
+//
+//  Created by Bryan de Bourbon on 3/23/25.
+//
+
+import Testing
+
+struct UserDefaultsProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
